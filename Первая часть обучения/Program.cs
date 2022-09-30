bc6eb0e92442acf809794e13198f6c457107bf8d
Liudmila <vocrug-solnza@mail.ru>
2022-09-30 16:52:31 +0300

    Методы

---
+++ Первая часть обучения/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Написать программу,которая из имеющегося массива строк формирует массив из строк,
+длина которых меньше или равна трем символам.Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма.При решении не рекомендуется пользоваться
+коллекциями,лучше обойтись исключительно массивами.*/
+
+string[] Words = new string [4]{"Yes", "No", "Always", "Never"};
+string[] Words1 = new string [Words.Length];
+
+
+
+void stringSize(string[] Words, string[] Words1)
+{
+     int count = 0;   
+     for(int i = 0; i < Words.Length; i++ )
+     {
+       if(Words[i].Length <= 3)
+       {
+         Words1[count] = Words[i];
+         count++;
+       }
+     }
+}
+
+void PrintArray(string[]Words)
+{
+   for(int i = 0; i < Words.Length; i++)
+   {
+
+    Console.Write($"|{Words[i]}| ");
+
+   }
+   Console.WriteLine();
+}
+
+stringSize(Words, Words1);
+PrintArray(Words);
+Console.WriteLine();
+
+PrintArray(Words1);   
