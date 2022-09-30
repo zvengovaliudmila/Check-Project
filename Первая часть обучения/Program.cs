0583ea19f9672acc7b741ece1e218e59b985711e
Liudmila <vocrug-solnza@mail.ru>
2022-09-30 17:15:00 +0300

    Создаем первый массив строк и второй массив,который принимает длину строк первого массива

--- Первая часть обучения/Program.cs
+++ Первая часть обучения/Program.cs
@@ -5,35 +5,3 @@
 
 string[] Words = new string [4]{"Yes", "No", "Always", "Never"};
 string[] Words1 = new string [Words.Length];
-
-
-
-void stringSize(string[] Words, string[] Words1)
-{
-     int count = 0;   
-     for(int i = 0; i < Words.Length; i++ )
-     {
-       if(Words[i].Length <= 3)
-       {
-         Words1[count] = Words[i];
-         count++;
-       }
-     }
-}
-
-void PrintArray(string[]Words)
-{
-   for(int i = 0; i < Words.Length; i++)
-   {
-
-    Console.Write($"|{Words[i]}| ");
-
-   }
-   Console.WriteLine();
-}
-
-stringSize(Words, Words1);
-PrintArray(Words);
-Console.WriteLine();
-
-PrintArray(Words1);   
